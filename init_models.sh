8fab1d7f327354a2080cdf1ef2d5f8245b0ea35c
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2023-12-29 20:46:16 +0900

    MAINT: init models script

---
+++ init_models.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "[01/03] 📂 Preparing folders..."
+mkdir -p checkpoints
+mkdir -p whisper
+echo "[01/03] ✅ Folders created!"
+
+echo "[02/03] 🎤 Downloading VALL-E-X checkpoint..."
+curl -o checkpoints/vallex-checkpoint.pt https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt
+echo "[02/03] ✅ VALL-E-X checkpoint downloaded!"
+
+echo "[03/03] 🤫 Downloading Whisper checkpoint..."
+curl -o checkpoints/vallex-checkpoint.pt https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt
+echo "[03/03] ✅ VALL-E-X checkpoint downloaded!"
