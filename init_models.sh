1c71933cc32082ca755eaf7643e81ed956aee27b
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2023-12-29 23:01:29 +0900

    FIX: whisper path in model init

--- init_models.sh
+++ init_models.sh
@@ -13,6 +13,6 @@ echo "[02/03] ✅ VALL-E-X checkpoint downloaded!"
 
 echo "[03/03] 🤫 Downloading Whisper checkpoint..."
 curl \
-    -o checkpoints/vallex-checkpoint.pt \
+    -o whisper/medium.pt \
     -L https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt
 echo "[03/03] ✅ VALL-E-X checkpoint downloaded!"
