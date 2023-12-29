40dc72417298e71326948fed6d0da4162c65f89d
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2023-12-29 22:14:13 +0900

    FIX: use HF link instead of dead OpenAI link for ckpt

--- init_models.sh
+++ init_models.sh
@@ -6,9 +6,13 @@ mkdir -p whisper
 echo "[01/03] ✅ Folders created!"
 
 echo "[02/03] 🎤 Downloading VALL-E-X checkpoint..."
-curl -o checkpoints/vallex-checkpoint.pt https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt
+curl \
+    -o checkpoints/vallex-checkpoint.pt \
+    -L https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt
 echo "[02/03] ✅ VALL-E-X checkpoint downloaded!"
 
 echo "[03/03] 🤫 Downloading Whisper checkpoint..."
-curl -o checkpoints/vallex-checkpoint.pt https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt
+curl \
+    -o checkpoints/vallex-checkpoint.pt \
+    -L https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt
 echo "[03/03] ✅ VALL-E-X checkpoint downloaded!"
